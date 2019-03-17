5ea4195b44d23dce01242ed8bc64c7d95f9f7d27
ArdiMaster <ardimaster@outlook.de>
2019-03-17 14:26:35 +0100

    Actually use NumericTableItem for PowerRatingView

--- src/powerrating_view.cpp
+++ src/powerrating_view.cpp
@@ -8,6 +8,7 @@
 
 #include "model/galaxy_state.h"
 #include "model/empire.h"
+#include "numerictableitem.h"
 
 PowerRatingView::PowerRatingView(QWidget *parent) : QTableWidget(parent) {
 	setColumnCount(4);
@@ -24,11 +25,11 @@ void PowerRatingView::modelChanged(const Galaxy::State *newState) {
 	for (auto it = empires.cbegin(); it != empires.cend(); it++) {
 		QTableWidgetItem *itemName = new QTableWidgetItem(it.value()->getName());
 		setItem(i, 0, itemName);
-		QTableWidgetItem *itemMilitary = new QTableWidgetItem(tr("%1").arg(it.value()->getMilitaryPower()));
+		NumericTableItem *itemMilitary = new NumericTableItem(tr("%1").arg(it.value()->getMilitaryPower()));
 		setItem(i, 1, itemMilitary);
-		QTableWidgetItem *itemEconomy = new QTableWidgetItem(tr("%1").arg(it.value()->getEconomyPower()));
+		NumericTableItem *itemEconomy = new NumericTableItem(tr("%1").arg(it.value()->getEconomyPower()));
 		setItem(i, 2, itemEconomy);
-		QTableWidgetItem *itemTechnology = new QTableWidgetItem(tr("%1").arg(it.value()->getTechPower()));
+		NumericTableItem *itemTechnology = new NumericTableItem(tr("%1").arg(it.value()->getTechPower()));
 		setItem(i++, 3, itemTechnology);
 	}
 	setSortingEnabled(true);
